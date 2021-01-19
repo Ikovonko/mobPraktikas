4cda1fbd07688110b9b9478b690a980937eb4a51
Ikovonko <74784317+Ikovonko@users.noreply.github.com>
2021-01-19 17:00:15 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,171 @@
+var pwaCard = document.querySelector('#pwa');
+var pwaCardContent = pwaCard.querySelector('.card__content');
+var pwaCardDetails = pwaCard.querySelector('.card__details');
+var detailsShown = false;
+
+pwaCard.addEventListener('click', function (event) {
+  if (!detailsShown) {
+    detailsShown = true;
+    pwaCardContent.style.opacity = 0;
+    pwaCardDetails.style.display = 'block';
+    pwaCardContent.style.display = 'none';
+    setTimeout(function () {
+      pwaCardDetails.style.opacity = 1;
+    }, 300);
+  } else {
+    detailsShown = false;
+    pwaCardDetails.style.opacity = 0;
+    pwaCardContent.style.display = 'block';
+    pwaCardDetails.style.display = 'none';
+    setTimeout(function () {
+      pwaCardContent.style.opacity = 1;
+    }, 300);
+  }
+});
+function getUserMedia(constraints) {
+  // if Promise-based API is available, use it
+  if (navigator.mediaDevices) {
+    return navigator.mediaDevices.getUserMedia(constraints);
+  }
+    
+  // otherwise try falling back to old, possibly prefixed API...
+  var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    
+  if (legacyApi) {
+    // ...and promisify it
+    return new Promise(function (resolve, reject) {
+      legacyApi.bind(navigator)(constraints, resolve, reject);
+    });
+  }
+}
+
+function getStream (type) {
+  if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+    alert('User Media API not supported.');
+    return;
+  }
+
+  var constraints = {};
+  constraints[type] = true;
+  
+  getUserMedia(constraints)
+    .then(function (stream) {
+      var mediaControl = document.querySelector(type);
+      
+      if ('srcObject' in mediaControl) {
+        mediaControl.srcObject = stream;
+      } else if (navigator.mozGetUserMedia) {
+        mediaControl.mozSrcObject = stream;
+      } else {
+        mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+      }
+      
+      mediaControl.play();
+    })
+    .catch(function (err) {
+      alert('Error: ' + err);
+    });
+  }
+
+  document.getElementById('status').innerHTML = navigator.onLine ? 'online' : 'offline';
+
+var target = document.getElementById('target');
+
+function handleStateChange() {
+  var timeBadge = new Date().toTimeString().split(' ')[0];
+  var newState = document.createElement('p');
+  var state = navigator.onLine ? 'online' : 'offline';
+  newState.innerHTML = '' + timeBadge + ' Connection state changed to ' + state + '.';
+  target.appendChild(newState);
+}
+
+window.addEventListener('online', handleStateChange);
+window.addEventListener('offline', handleStateChange);
+
+
+
+if ('getBattery' in navigator || ('battery' in navigator && 'Promise' in window)) {
+  var target = document.getElementById('target');
+
+  function handleChange(change) {
+    var timeBadge = new Date().toTimeString().split(' ')[0];
+    var newState = document.createElement('p');
+    newState.innerHTML = '' + timeBadge + ' ' + change + '.';
+    target.appendChild(newState);
+  }
+  
+  function onChargingChange() {
+    handleChange('Battery charging changed to ' + (this.charging ? 'charging' : 'discharging') + '')
+  }
+  function onChargingTimeChange() {
+    handleChange('Battery charging time changed to ' + this.chargingTime + ' s');
+  }
+  function onDischargingTimeChange() {
+    handleChange('Battery discharging time changed to ' + this.dischargingTime + ' s');
+  }
+  function onLevelChange() {
+    handleChange('Battery level changed to ' + this.level + '');
+  }
+
+  var batteryPromise;
+  
+  if ('getBattery' in navigator) {
+    batteryPromise = navigator.getBattery();
+  } else {
+    batteryPromise = Promise.resolve(navigator.battery);
+  }
+  
+  batteryPromise.then(function (battery) {
+    document.getElementById('charging').innerHTML = battery.charging ? 'charging' : 'discharging';
+    document.getElementById('chargingTime').innerHTML = battery.chargingTime + ' s';
+    document.getElementById('dischargingTime').innerHTML = battery.dischargingTime + ' s';
+    document.getElementById('level').innerHTML = battery.level;
+    
+    battery.addEventListener('chargingchange', onChargingChange);
+    battery.addEventListener('chargingtimechange', onChargingTimeChange);
+    battery.addEventListener('dischargingtimechange', onDischargingTimeChange);
+    battery.addEventListener('levelchange', onLevelChange);
+  });
+
+  function isSuccessful(response) {
+  return response &&
+    response.status === 200 &&
+    response.type === 'basic';
+}
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response; // Cache hit
+        }
+
+        return fetch(event.request.clone())
+          .then(function (response) {
+            if (!isSuccessful(response)) {
+              return response;
+            }
+
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                cache.put(event.request, response.clone());
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('my-cache-v1')
+      .then(function (cache) {
+        return cache.addAll(['/', '/styles/main.css', '/scripts/main.js']);
+      })
+  );
+});
